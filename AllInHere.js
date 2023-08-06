bdf479746b2905e6069df8d3d81fff3a3e3c397e
home-github <124263428+home-gihub@users.noreply.github.com>
2023-08-06 13:54:30 +0100

    Add files via upload

---
+++ AllInHere.js
@@ -0,0 +1,14 @@
+// AllInHere.js, a very simple way to not have to touch index.html and just code in js
+console.log('AllInHere.js, a very simple way to not have to touch index.html and just code in js')
+
+// Get the functions we need from App.js
+import * as app from "App.js"
+// Initalize the root div element
+RootElement = document.createElement('div')
+RootElement.id = "App"
+document.body.appendChild(RootElement)
+// Add the contents from the Init function in App.js
+document.getElementById("App").innerHTML = app.Init()
+// Run the scripts in the App function in App.js
+app.App()
+// We are all done here
---
+++ App.js
@@ -0,0 +1,13 @@
+export function App() {
+    var counter = 0
+    function count() {
+        counter++
+        document.getElementById('counter').innerHTML = `count is ${counter}`
+    }
+    document.getElementById('App').append('<button id="counter" onclick="count()">Count is 0</button>')
+}
+
+export function Init() {
+    Body = "<h1>it is just all in here</h1>"
+    return Body;
+}
