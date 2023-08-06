ad86241a480ead280ecd286b1984bd9819103fe6
home-github <124263428+home-gihub@users.noreply.github.com>
2023-08-06 14:37:18 +0100

    j

--- AllInHere.mjs
+++ AllInHere.mjs
@@ -4,10 +4,7 @@ console.log('AllInHere.js, a very simple way to not have to touch index.html and
 // Get the functions we need from App.js
 import * as app from "./App.js"
 // Initalize the root div element
-AIHRootElement = ""
-AIHRootElement = document.createElement('div')
-AIHRootElement.id = "App"
-document.body.appendChild(AIHRootElement)
+document.body.innerHTML = '<div id="App"></div>'
 // Add the contents from the Init function in App.js
 document.getElementById("App").innerHTML = app.Init()
 // Run the scripts in the App function in App.js
