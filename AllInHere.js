49e7ac1b1b8858d8b4e3aa084445ccab6939dcc9
home-github <124263428+home-gihub@users.noreply.github.com>
2023-08-06 14:51:31 +0100

    Update and rename AllInHere.mjs to AllInHere.js

--- AllInHere.js
+++ AllInHere.js
@@ -1,12 +1,10 @@
 // AllInHere.js, a very simple way to not have to touch index.html and just code in js
 console.log('AllInHere.js, a very simple way to not have to touch index.html and just code in js')
 
-// Get the functions we need from App.js
-import * as app from "App.js"
 // Initalize the root div element
 document.body.innerHTML = '<div id="App"></div>'
-// Add the contents from the Init function in App.js
-document.getElementById("App").innerHTML = app.Init()
-// Run the scripts in the App function in App.js
-app.App()
+// Add the contents from the Init function
+document.getElementById("App").innerHTML = Init()
+// Run the scripts in the App function
+App()
 // We are all done here
