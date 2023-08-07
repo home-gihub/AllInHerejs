5131b753a7c08f33221010c9140ce05c0c8125b7
home-github <124263428+home-gihub@users.noreply.github.com>
2023-08-07 20:09:47 +0100

    Update AllInHere.js

--- AllInHere.js
+++ AllInHere.js
@@ -1,16 +1,18 @@
-// AllInHere.js, a very simple way to not have to touch index.html and just code in js
-console.log('AllInHere.js, a very simple way to not have to touch index.html and just code in js')
+console.log('AllInHere.js ----------)
 // so you can refresh all of the interactive content
-function Refresh() {
-    document.getElementById("App").innerHTML = App().Body
-}
+function Refresh() {document.getElementById("App").innerHTML = App().Body}
 // Null fix
+console.log('fixing null...')
 const fix = document.createElement('div');
 document.body.appendChild(fix);
+console.log('adding app div...')
 // Initalize the root div element
 document.body.innerHTML = '<div id="App"></div>'
 // Add the contents from the Init function
+console.log('adding page...')
 document.getElementById("App").innerHTML = App().Body
 // Run the scripts in the App function
+console.log('running scripts...')
 App()
 // We are all done here
+console.log('--------------------')
