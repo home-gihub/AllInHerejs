22a7694676fe71167d512a133e9276a9515da3b3
home-github <124263428+home-gihub@users.noreply.github.com>
2023-08-08 12:48:46 +0100

    Update AllInHere.js

--- AllInHere.js
+++ AllInHere.js
@@ -1,18 +1,18 @@
-console.log('%cAllInHere.js ----------', 'color: green; background: black;')
+console.log('%cAllInHere.js ----------')
 // so you can refresh all of the interactive content
 function Refresh() {document.getElementById("App").innerHTML = App().Body}
 // Null fix
-console.log('fixing null...', 'color: green; background: black;')
+console.log('fixing null...')
 const fix = document.createElement('div');
 document.body.appendChild(fix);
-console.log('adding app div...', 'color: green; background: black;')
+console.log('adding app div...')
 // Initalize the root div element
 document.body.innerHTML = '<div id="App"></div>'
 // Add the contents from the Init function
-console.log('adding page...', 'color: green; background: black;')
+console.log('adding page...')
 document.getElementById("App").innerHTML = App().Body
 // Run the scripts in the App function
-console.log('running scripts...', 'color: green; background: black;')
+console.log('running scripts...')
 App()
 // We are all done here
-console.log('--------------------', 'color: green; background: black;')
+console.log('--------------------')
